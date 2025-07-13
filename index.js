0aec594eb46f64053f8284873c016dafa66c4aa3
aleks <achudars@gmail.com>
2025-07-13 22:08:15 +0100

    refactor: remove jquery.lettering.js

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 $(document).ready(function () {
+  // Use mustache.js templating to create layout
+  var html = Mustache.to_html($('#template').html());
 
-    // Use mustache.js templating to create layout
-    var html = Mustache.to_html($('#template').html());
+  $('#main').replaceWith(html);
 
-    $('#main').replaceWith(html);
+  // For each image:
+  // Once image is loaded, get dominant color and palette and display them.
+  $('img').bind('load', function (event) {
+    // check if swatches already contain colours in them
+    if ($('.swatches').children().length >= 10) {
+      $('.swatches').children().slice(0, 10).remove();
+    }
+    var image = event.target;
+    var $image = $(image);
+    var imageSection = $image.closest('.imageSection');
+    var appendColors = function (colors, root) {
+      $.each(colors, function (index, value) {
+        var swatchEl = $('<div>', { class: 'swatch' }).css(
+          'background-color',
+          'rgba(' + value + ', 1)'
+        );
+        root.append(swatchEl);
+      });
+    };
 
-    // Use lettering.js to give letter by letter styling control for the h1 title
-    $("h1").lettering();
+    // Dominant Color
+    var dominantColor = getDominantColor(image);
+    var dominantSwatch = imageSection.find('.dominantColor .swatches');
+    appendColors([dominantColor], dominantSwatch);
 
-    // For each image:
-    // Once image is loaded, get dominant color and palette and display them.
-    $('img').bind('load', function (event) {
-        // check if swatches already contain colours in them
-        if ( $('.swatches').children().length >= 10 ) {
-            $(".swatches").children().slice(0, 10).remove();
-        }
-        var image = event.target;
-        var $image = $(image);
-        var imageSection = $image.closest('.imageSection');
-        var appendColors = function (colors, root) {
-            $.each(colors, function (index, value) {
-                var swatchEl = $('<div>', {'class': 'swatch'})
-                    .css('background-color', 'rgba('+ value +', 1)');
-                root.append(swatchEl);
-            });
-        };
-
-        // Dominant Color
-        var dominantColor = getDominantColor(image);
-        var dominantSwatch = imageSection.find('.dominantColor .swatches');
-        appendColors([dominantColor], dominantSwatch);
-
-        // Palette
-        var colorCount = $image.attr('data-colorcount') ? $image.data('colorcount') : 10;
-        var medianPalette = createPalette(image, colorCount);
-        var medianCutPalette = imageSection.find('.medianCutPalette .swatches');
-        appendColors(medianPalette, medianCutPalette);
-    });
+    // Palette
+    var colorCount = $image.attr('data-colorcount') ? $image.data('colorcount') : 10;
+    var medianPalette = createPalette(image, colorCount);
+    var medianCutPalette = imageSection.find('.medianCutPalette .swatches');
+    appendColors(medianPalette, medianCutPalette);
+  });
 });
